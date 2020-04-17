2bee1d794e7fbb0ab803b389d3796837b1091590
Martin Geisse <martingeisse@googlemail.com>
2020-04-17 23:01:11 +0200

    use explicit algorithm for fixedSqrt

--- fixed/src/Fixed.cpp
+++ fixed/src/Fixed.cpp
@@ -31,6 +31,17 @@ void printFixed(Fixed x) {
     printf("%f", fixedToFloat(x));
 }
 
-Fixed fixedSqrt(Fixed x) {
-    return floatToFixed(sqrtf(fixedToFloat(x)));
+Fixed fixedSqrt(Fixed xf) {
+    int x = xf.value;
+    if (x <= 0) {
+        return fixedZero;
+    }
+    int r  = x;
+    while (true) {
+        int next = (r + x / r) >> 1;
+        if (next >= r - 1 && next <= r + 1) {
+            return buildFixed(next << 8);
+        }
+        r = next;
+    }
 }
